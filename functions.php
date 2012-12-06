f61461e20982e16dbc3197c549e7f3d844c044b7
Michele Gobbi <info@dynamick.it>
2012-12-06 16:15:02 +0100

    cleaned unused functions

--- functions.php
+++ functions.php
@@ -14,6 +14,7 @@
 	// Load external files 
 	require_once dirname( __FILE__ ) . '/framework/boxed_text_widget.php';
 	require_once dirname( __FILE__ ) . '/framework/class-tgm-plugin-activation.php';
+	require_once dirname( __FILE__ ) . '/framework/bootstrap_walker_menu.php';
 
 /*
  * ========================================================================
@@ -70,168 +71,6 @@ if (function_exists('add_theme_support'))
  * Functions
  * ========================================================================
  */
-
-// Simple Perfect Coding navigation
-function perfectcoding_nav()
-{
-	wp_nav_menu(
-	array(
-		'theme_location'  => 'header-menu',
-		'menu'            => '', 
-		'container'       => 'div', 
-		'container_class' => 'menu-{menu slug}-container', 
-		'container_id'    => '',
-		'menu_class'      => 'menu', 
-		'menu_id'         => '',
-		'echo'            => true,
-		'fallback_cb'     => 'wp_page_menu',
-		'before'          => '',
-		'after'           => '',
-		'link_before'     => '',
-		'link_after'      => '',
-		'items_wrap'      => '<ul id="main">%3$s</ul>',
-		'depth'           => 0,
-		'walker'          => ''
-		)
-	);
-}
-
-/* Advanced Perfect Coding navigation: Bootstrap Menu */
-
-add_action( 'after_setup_theme', 'bootstrap_setup' );
-
-if ( ! function_exists( 'bootstrap_setup' ) ):
-
-	function bootstrap_setup(){
-
-		add_action( 'init', 'register_menu' );
-			
-		function register_menu(){
-			register_nav_menu( 'top-bar', 'Bootstrap Top Menu' ); 
-		}
-
-		class Bootstrap_Walker_Nav_Menu extends Walker_Nav_Menu {
-
-			
-			function start_lvl( &$output, $depth ) {
-
-				$indent = str_repeat( "\t", $depth );
-				$output	   .= "\n$indent<ul class=\"dropdown-menu\">\n";
-				
-			}
-
-			function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-				
-				$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-
-				$li_attributes = '';
-				$class_names = $value = '';
-
-				$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-				$classes[] = ($args->has_children) ? 'dropdown' : '';
-				$classes[] = ($item->current || $item->current_item_ancestor) ? 'active' : '';
-				$classes[] = 'menu-item-' . $item->ID;
-
-
-				$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-				$class_names = ' class="' . esc_attr( $class_names ) . '"';
-
-				$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
-				$id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
-
-				$output .= $indent . '<li' . $id . $value . $class_names . $li_attributes . '>';
-
-				$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-				$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-				$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-				$attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-				$attributes .= ($args->has_children) 	    ? ' class="disabled dropdown-toggle" data-toggle="dropdown" ' : '';
-
-				$item_output = $args->before;
-				$item_output .= '<a'. $attributes .'>';
-				$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-				$item_output .= ($args->has_children) ? ' <b class="caret"></b></a>' : '</a>';
-				$item_output .= $args->after;
-				$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-			}
-
-			function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
-				
-				if ( !$element )
-					return;
-				
-				$id_field = $this->db_fields['id'];
-
-				//display this element
-				if ( is_array( $args[0] ) ) 
-					$args[0]['has_children'] = ! empty( $children_elements[$element->$id_field] );
-				else if ( is_object( $args[0] ) ) 
-					$args[0]->has_children = ! empty( $children_elements[$element->$id_field] ); 
-				$cb_args = array_merge( array(&$output, $element, $depth), $args);
-				call_user_func_array(array(&$this, 'start_el'), $cb_args);
-
-				$id = $element->$id_field;
-
-				// descend only when the depth is right and there are childrens for this element
-				if ( ($max_depth == 0 || $max_depth > $depth+1 ) && isset( $children_elements[$id]) ) {
-
-					foreach( $children_elements[ $id ] as $child ){
-
-						if ( !isset($newlevel) ) {
-							$newlevel = true;
-							//start the child delimiter
-							$cb_args = array_merge( array(&$output, $depth), $args);
-							call_user_func_array(array(&$this, 'start_lvl'), $cb_args);
-						}
-						$this->display_element( $child, $children_elements, $max_depth, $depth + 1, $args, $output );
-					}
-						unset( $children_elements[ $id ] );
-				}
-
-				if ( isset($newlevel) && $newlevel ){
-					//end the child delimiter
-					$cb_args = array_merge( array(&$output, $depth), $args);
-					call_user_func_array(array(&$this, 'end_lvl'), $cb_args);
-				}
-
-				//end this element
-				$cb_args = array_merge( array(&$output, $element, $depth), $args);
-				call_user_func_array(array(&$this, 'end_el'), $cb_args);
-				
-			}
-			
-		}
-
-	}
-
-endif;
-
-
-# Activate this hook to add a logo inside the menu
-	
-#add_filter('wp_nav_menu_items', 'add_logo_to_nav', 10, 2);
-function add_logo_to_nav($items, $args)
-{
-	if ($args->theme_location == 'header-menu') {
-		$url =get_template_directory_uri();
-		$logo = <<<EOF
-			<li id="logo-small" >
-				<a href="/" ><img src="{$url}/img/logo-dynamick.png" alt="Dynamick, Web design, develop and SEO"></a>
-			</li>
-EOF;
-		$items = "{$logo}".$items;
-	}
-    return $items;
-}
-
-# activate this hook to add a special class to the menu item
-
-#add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
-function special_nav_class($classes, $item){
-     $classes[] = "span1";
-     return $classes;
-}
-
 
 // Load Custom Theme Scripts using Enqueue
 function perfectcoding_scripts()
@@ -374,17 +213,20 @@ function perfectcoding_styles()
 	    wp_enqueue_style('open_sans'); // Enqueue it!
 }
 
-// Register Perfect Coding's Navigation
+// Register Perfect Coding's Navigation menu, other than header menu defined above
 function register_perfectcoding_menu()
-{
+{	
     register_nav_menus(array( // Using array to specify more menus if needed
-        'header-menu' => __('Header Menu', 'perfectcoding'), // Main Navigation
         'sidebar-menu' => __('Sidebar Menu', 'perfectcoding'), // Sidebar Navigation
         'footer-col1' => __('Footer col1 Menu', 'perfectcoding'), // Footer Col1 Menu
         'footer-col2' => __('Footer col2 Menu', 'perfectcoding'), // Footer Col2 Menu
         'footer-col3' => __('Footer col3 Menu', 'perfectcoding') // Footer Col3 Menu
     ));
 }
+
+/* Advanced Perfect Coding navigation: Bootstrap Menu */
+add_action( 'after_setup_theme', 'bootstrap_setup' );
+
 
 // Remove the <div> surrounding the dynamic navigation to cleanup markup
 function my_wp_nav_menu_args($args = '')
@@ -428,7 +270,7 @@ if (function_exists('register_sidebar'))
 {
     // Define Sidebar Widget Area 1 - sidebar top
     register_sidebar(array(
-        'name' => __('Widget Area 1', 'perfectcoding'),
+        'name' => __('Top Sidebar', 'perfectcoding'),
         'description' => __('Top part of the sidebar', 'perfectcoding'),
         'id' => 'widget-area-1',
         'before_widget' => '<div id="%1$s" class="%2$s">',
@@ -439,7 +281,7 @@ if (function_exists('register_sidebar'))
 
     // Define Sidebar Widget Area 2 - sidebar middle
     register_sidebar(array(
-        'name' => __('Widget Area 2', 'perfectcoding'),
+        'name' => __('Bottom Sidebar', 'perfectcoding'),
         'description' => __('Bottom part of the sidebar', 'perfectcoding'),
         'id' => 'widget-area-2',
         'before_widget' => '<div id="%1$s" class="%2$s">',
@@ -450,7 +292,7 @@ if (function_exists('register_sidebar'))
 
     // Define Sidebar Widget Area 3 - footer 1° col
     register_sidebar(array(
-        'name' => __('Widget Area 3', 'perfectcoding'),
+        'name' => __('Footer left', 'perfectcoding'),
         'description' => __('Footer left area', 'perfectcoding'),
         'id' => 'widget-area-3',
         'before_widget' => '<div id="%1$s" class="%2$s">',
@@ -461,7 +303,7 @@ if (function_exists('register_sidebar'))
 
     // Define Sidebar Widget Area 4 - footer 2° col
     register_sidebar(array(
-        'name' => __('Widget Area 4', 'perfectcoding'),
+        'name' => __('Footer Middle', 'perfectcoding'),
         'description' => __('Footer middle area', 'perfectcoding'),
         'id' => 'widget-area-4',
         'before_widget' => '<div id="%1$s" class="%2$s">',
@@ -472,7 +314,7 @@ if (function_exists('register_sidebar'))
 
     // Define Sidebar Widget Area 5 - footer 3° col
     register_sidebar(array(
-        'name' => __('Widget Area 5', 'perfectcoding'),
+        'name' => __('Footer Right', 'perfectcoding'),
         'description' => __('Footer right area', 'perfectcoding'),
         'id' => 'widget-area-5',
         'before_widget' => '<div id="%1$s" class="%2$s">',
