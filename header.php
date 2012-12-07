27f9182c77c45a4d10592d573141c49556d060b8
Michele Gobbi <info@dynamick.it>
2012-12-07 16:47:03 +0100

    ie css

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if IE 8]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]--> 
+<!--[if gt IE 8]><html <?php language_attributes(); ?> class="no-js"><![endif]--> 
 <head>
 	<meta charset="UTF-8">
 	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
